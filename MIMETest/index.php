8b82f8036ba437cf3bf4aa2ea6afce00ab797a29
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2023-11-04 09:36:11 +0100

    Quick fix 'cause I was drunk. Joking. Just decided to change some stupid texts in tests. ;)

--- MIMETest/index.php
+++ MIMETest/index.php
@@ -3,9 +3,9 @@
 header('Content-Type: application/json');
 // header('Content-Disposition: attachment; filename=test.json; Content-Type: application/json');
 
-$test3=array('lol' => 'no tak','i' => 'co?');
-$test2=array('ha',$test3,'hu');
-$test=array('To' => 'cycki','prosty' => 'kutas','test' => $test2);
+$test3=array('test1' => 'test2','test3' => 'test4');
+$test2=array('test5',$test3,'test6');
+$test=array('test7' => 'test8','test9' => 'test10','test11' => $test2);
 
 echo json_encode($test);
 
